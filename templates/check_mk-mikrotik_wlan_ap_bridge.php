195ff6b5e3c9aaf77f5d6b53cb600b0e49f068b2
Łukasz Górski <moon.musick@gmail.com>
2015-04-22 21:06:58 +0200

    Fix PNP4Nagios template
    Modified template to use PNP4Nagios helper functions exclusively.

--- templates/check_mk-mikrotik_wlan_ap_bridge.php
+++ templates/check_mk-mikrotik_wlan_ap_bridge.php
@@ -6,11 +6,10 @@ $def[1] .= rrd::hrule($WARN[1], "#FFFF00", "Warning");
 $def[1] .= rrd::hrule($CRIT[1], "#FF0000", "Critical\l");
 $def[1] .= rrd::area("clients", "#8FBBC4", "Clients");
 $def[1] .= rrd::line1("clients", "#000000");
-$def[1] .= "GPRINT:clients:LAST:\"Last %2.0lf\" ";
-$def[1] .= "GPRINT:clients:MIN:\"Min %2.0lf\" ";
-$def[1] .= "GPRINT:clients:MAX:\"Max %2.0lf\" ";
-$def[1] .= "GPRINT:clients:AVERAGE:\"Average %2.2lf\" ";
-
+$def[1] .= rrd::gprint("clients", "LAST",     "Last %2.0lf");
+$def[1] .= rrd::gprint("clients", "MIN",      "\tMin %2.0lf");
+$def[1] .= rrd::gprint("clients", "MAX",      "\tMax %2.0lf");
+$def[1] .= rrd::gprint("clients", "AVERAGE",  "\tAverage %2.0lf");
 
 $opt[2] =  "--vertical-label \"CCQ %\" --title \"CCQ for $hostname / $servicedesc\" ";
 
@@ -18,8 +17,8 @@ $def[2] =  rrd::def("ccq", $RRDFILE[2], $DS[2], "AVERAGE");
 $def[2] .= rrd::hrule($WARN[2], "#FFFF00", "Warning");
 $def[2] .= rrd::hrule($CRIT[2], "#FF0000", "Critical\l");
 $def[2] .= rrd::gradient("ccq", "#FFFFFF", "#41BF91", "CCQ");
-$def[2] .= "GPRINT:ccq:LAST:\"Last %2.0lf%%\" ";
-$def[2] .= "GPRINT:ccq:MIN:\"Min %2.0lf%%\" ";
-$def[2] .= "GPRINT:ccq:MAX:\"Max %2.0lf%%\" ";
-$def[2] .= "GPRINT:ccq:AVERAGE:\"Average %2.0lf%%\" ";
+$def[2] .= rrd::gprint("ccq", "LAST",     "Last %2.0lf%%");
+$def[2] .= rrd::gprint("ccq", "MIN",      "\tMin %2.0lf%%");
+$def[2] .= rrd::gprint("ccq", "MAX",      "\tMax %2.0lf%%");
+$def[2] .= rrd::gprint("ccq", "AVERAGE",  "\tAverage %2.0lf%%");
 ?>
